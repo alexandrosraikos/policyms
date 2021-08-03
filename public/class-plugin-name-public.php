5fd8a0b2960810ead79c081b0f8a982e6660017f
elefkour <elkour@unipi.gr>
2021-08-03 20:52:22 +0300

    oi allages mou :D

--- public/class-plugin-name-public.php
+++ public/class-plugin-name-public.php
@@ -127,7 +127,39 @@ class Plugin_Name_Public
 		 * Αλλιώς βγάζει σφάλμα η συνάρτηση:
 		 * throw new Exception("μήνυμα");
 		 */
+		//to evala se afth th metavlhth epeidh eixa kanei etsi sthn arxh tous elenxous.
+	
+		$data = $information;
 
+		if (empty($data['username']) ||
+			empty($data['password']) ||
+			empty($data['email']) ||
+			empty($data['name']) ||
+			empty($data['surname']) ||
+			empty($data['title']) ||
+			empty($data['gender'])||
+			empty($data['organization']) ||
+			empty($data['phone'])) {
+			
+			throw new Exception('Please fill all required fields!');
+		}
+		if(!filter_var($data["email"], FILTER_VALIDATE_EMAIL)){
+			throw new Exception("Please enter a valid email");
+		}
+		//afto to evala mh to xreiastoume kapoia stigmh gia antistoixia kwdikwn
+		if ($data['password'] !== $data['password_confirm']) {
+		   throw new Exception('Password and Confirm password should match!');   
+		   
+		}
+		$uppercase = preg_match('@[A-Z]@', $data['password']);
+$lowercase = preg_match('@[a-z]@', $data['password']);
+$number    = preg_match('@[0-9]@', $data['password']);
+$specialChars = preg_match('@[^\w]@', $data['password']);
+//se periptwsh pou theloume na kanoume to xrhsth na valei isxuro kwdiko
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($data['password']) < 8) {
+    throw new Exception('Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.');
+}
+		
 		/**
 		 * TODO @elefkour : Αποστολή με HTTP POST στο Marketplace API.
 		 * Προβολή στοιχείων χρήστη εδώ:
@@ -137,6 +169,26 @@ class Plugin_Name_Public
 		 * - Σε περίπτωση επιτυχίας, τότε:
 		 * Βάλτο στο $response.
 		 */
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'https://iristest.ddns.net:4444/registration/users',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'POST',
+		  CURLOPT_POSTFIELDS =>'{"password": "'.$data['password'].'", "username": "'.$data['username'].'", "name": "'.$data['name'].'", "surname": "'.$data['surname'].'", "title": "'.$data['title'].'", "gender": "'.$data['gender'].'", "organization": "'.$data['organization'].'", "email": "'.$data['email'].'", "phone": "'.$data['phone'].'"}',
+		  CURLOPT_HTTPHEADER => array('Content-Type: application/json')
+		));
+		
+		$response = json_decode(curl_exec($curl),true);
+		curl_close($curl);
+		if ($response['_status'] == "successful")
+			return array($response["token"], 1);
+		else return array($response["message"], 0, $response);
 
 		/**
 		 * TODO @alexandrosraikos : Αποθήκευση κρυπτογραφημένου JWT.
